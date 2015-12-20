bacb50c27a07526e5fdf889b2f16b6c2d12d701b
Cuong Luong <ndrw_luong@yahoo.com>
2015-12-19 16:03:44 -0800

    Used Fast Marching Method to get a better external skin layer
    Also used contouring to include edge points with normal vectors facing
    outwards

--- thickness_smoothing.m
+++ thickness_smoothing.m
@@ -1,12 +1,14 @@
 function [new_thickness] = thickness_smoothing(thickness)
-% Remove anomalous outlier thickness values 
+% Remove anomalous outlier thickness values
 
 mavg = 5;
 new_thickness = thickness;
 
 for ii = 1+mavg:length(thickness)-mavg
-    if thickness(ii) > mean(thickness(ii-mavg:ii+mavg))
-        new_thickness(ii) = mean(thickness(ii-mavg:ii-1));
+    if thickness(ii) > mean(thickness(ii-mavg:ii+mavg)) + 0.5*std(thickness(ii-mavg:ii+mavg))
+        %         new_thickness(ii) = mean(thickness(ii-mavg:ii-1));
+        new_thickness(ii) = new_thickness(ii-1);
+        
     else
         new_thickness(ii) = thickness(ii);
     end
